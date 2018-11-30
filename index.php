7adf61e5b28e2041d4000b52a18be32adf3c34ba
Ariel Ruiz Mateos <ariel.ruizmateos@arba.gov.ar>
2018-11-30 12:09:39 -0300

    modificacion mockup

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
   $data = array();
-  $data["nombre"] = "ariel ivan ruiz mateos";
-  $data["deuda"] = "5345,66";
+  $data["nombre"] = "JORGE SANTOS";
+  $data["cuit"] = "20888888880";
+  $data["deuda_total"] = "5345,66";
   echo json_encode($data, JSON_PRETTY_PRINT);
 
   ?>
